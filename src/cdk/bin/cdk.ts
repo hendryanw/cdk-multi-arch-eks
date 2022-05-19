68c56720384f2cb88f5f33932879e8ba3c56dc49
hendryanw <hendry.anwar@live.com>
2022-05-19 17:49:55 +0700

    multi arch image pipeline cdk stack

--- src/cdk/bin/cdk.ts
+++ src/cdk/bin/cdk.ts
@@ -2,13 +2,22 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EksInfraMultiArchStack } from '../lib/eks-infra-multi-arch-stack';
+import { MultiArchImagePipelineStack } from '../lib/multi-arch-image-pipeline-stack';
 
 const app = new cdk.App();
 new EksInfraMultiArchStack(app, 'eks-infra-multi-arch-stack', {
-  clusterName: 'multi-arch-eks-cluster',
-  vpcId: 'vpc-0ac9149c85f34c8f8',
+  clusterName: app.node.tryGetContext('app-config/clusterName'),
+  vpcId: app.node.tryGetContext('app-config/vpcId'),
   env: { 
     account: process.env.CDK_DEFAULT_ACCOUNT, 
-    region: 'ap-southeast-1'
+    region: process.env.CDK_DEFAULT_REGION
   }
+});
+
+new MultiArchImagePipelineStack(app, 'multi-arch-image-pipeline-stack', {
+  ecrRepositoryName: app.node.tryGetContext('app-config/ecrRepositoryName'),
+  gitHubRepo: app.node.tryGetContext('app-config/gitHubRepo'),
+  gitHubRepoBranchName: app.node.tryGetContext('app-config/gitHubRepoBranchName'),
+  gitHubRepoOwner: app.node.tryGetContext('app-config/gitHubRepoOwner'),
+  gitHubTokenAwsSecretsName: app.node.tryGetContext('app-config/gitHubTokenAwsSecretsName')
 });
